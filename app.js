f744d6bcb85c3135320b0ad9a1d57f4367c8ff38
hritikb27 <hritikb90@gmail.com>
2021-09-25 17:03:09 +0530

    Can do multiple Equations

--- app.js
+++ app.js
@@ -1,79 +1,85 @@
+//Variable Declared
 const getValue = document.querySelector('#calc-display');
 const submit = document.querySelector('#submit');
 const digits = document.querySelectorAll('.digits');
+const clear = document.querySelector('#clear');
+const back = document.querySelector('#back');
+const operators = document.querySelectorAll('.operators');
 
+//For Multiple Equations
+operators.forEach(operator=>{
+    operator.addEventListener('click', ()=>{
+        if(getValue.value.includes('+')||getValue.value.includes('-')||getValue.value.includes('*')||getValue.value.includes('/')){
+            getSumValue();
+        }
+    })
+})
+
+//For displaying operators
 digits.forEach(digit=>{
     digit.addEventListener('click', ()=>{
         getValue.value += digit.getAttribute('value');
     })
 })
 
+//Basic Calculator Functions
 function add(a,b){
     const c = parseInt(a);
     const d = parseInt(b);
-    console.log(c+d);
+    return(c+d);
 };
 
 function subtract(a,b){
     const c = parseInt(a);
     const d = parseInt(b);
-    console.log(c-d);
-    return sum;
+    return (c-d);
 };
 
 function multiply(a,b){
     const c = parseInt(a);
     const d = parseInt(b);
-    console.log(c*d);
-    // return sum;
+    return (c*d);
 };
 
 function divide(a,b){
     const c = parseInt(a);
     const d = parseInt(b);
-    console.log(c/d);
-    return sum;
+    return (c/d);
 };
 
-function operate(sum){
-    const detect = sum;
-    console.log('hello')
-    if(detect.includes('+')){
-        const a = sum.split('+');
-        console.log(a)
-        add(a[0],a[1]);
-    }
-    else if(detect.includes('-')){
-        const a = sum.split('-');
-        subtract(a[0],a[1]);
-    }
-    else if(detect.includes('*')){
-        const a = sum.split('*');
-        multiply(a[0],a[1]);
-    }
-    else if(detect.includes('/')){
-        const a = sum.split('/');
-        divide(a[0],a[1]);
-    }
-}
-
-submit.addEventListener('click', ()=>{
+//Calculation Algorithm
+function getSumValue(){
     const detect = getValue.value;
     if(detect.includes('+')){
         const a = detect.split('+');
-        console.log(a)
-        add(a[0],a[1]);
+        const sum = add(a[0],a[1]);
+        getValue.value = sum;
     }
     else if(detect.includes('-')){
         const a = detect.split('-');
-        subtract(a[0],a[1]);
+        const sum = subtract(a[0],a[1]);
+        getValue.value = sum;
     }
     else if(detect.includes('*')){
         const a = detect.split('*');
-        multiply(a[0],a[1]);
+        const sum = multiply(a[0],a[1]);
+        getValue.value = sum;
     }
     else if(detect.includes('/')){
         const a = detect.split('/');
-        divide(a[0],a[1]);
+        const sum = divide(a[0],a[1]);
+        getValue.value = sum;
     }
+}
+
+
+//Event Listeners
+submit.addEventListener('click', getSumValue);
+
+clear.addEventListener('click', ()=>{
+    getValue.value = '';
+})
+
+back.addEventListener('click', ()=>{
+    getValue.value = getValue.value.substr(0, getValue.value.length-1);
 })
